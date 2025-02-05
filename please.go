4bb49ec3426541a31b626a4bee2fd5fc7a373945
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2025-02-05 04:32:31 +0000

    Added more actions to please

--- please.go
+++ please.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 type ActionResult uint8
-
-const RESULT_SUCCESS ActionResult = 0
-const RESULT_INVALID_USAGE ActionResult = 255
-
 type ActionFunction func(args []string) ActionResult
 type Action struct {
 	description string
@@ -17,16 +18,56 @@ type Action struct {
 	action      ActionFunction
 }
 
+const RESULT_SUCCESS ActionResult = 0
+const RESULT_FAILURE ActionResult = 1
+const RESULT_INVALID_USAGE ActionResult = 255
+
 var actions map[string]Action = map[string]Action{
 	"start": {
 		description: "Starts given stack",
 		usage:       "start <stack_name> [options]",
 		action: func(args []string) ActionResult {
-			if len(args) == 0 {
+			if len(args) < 1 {
 				return RESULT_INVALID_USAGE
 			}
 
-			return RESULT_SUCCESS
+			return runCommand("./stacks/"+args[0], "docker compose up -d --remove-orphans", nil, nil)
+		},
+	},
+	"restart": {
+		description: "Restarts given service of the given stack",
+		usage:       "restart <stack_name> <service_name> [anoter_service_names]",
+		action: func(args []string) ActionResult {
+			if len(args) < 2 {
+				return RESULT_INVALID_USAGE
+			}
+
+			actionResult := RESULT_SUCCESS
+
+			serviceNames := args[1:]
+			longestNameLen := 0
+
+			for _, serviceName := range serviceNames {
+				if len(serviceName) > longestNameLen {
+					longestNameLen = len(serviceName)
+				}
+			}
+
+			format := fmt.Sprintf("%%-%ds %%s\n", longestNameLen+1)
+
+			for _, serviceName := range serviceNames {
+				commandResult := runCommand("./stacks/"+args[0], fmt.Sprintf("docker compose restart %s", serviceName), nil, nil)
+				commandResultString := "Done"
+
+				if commandResult != RESULT_SUCCESS {
+					actionResult = RESULT_FAILURE
+					commandResultString = "Failed"
+				}
+
+				fmt.Printf(format, serviceName+":", commandResultString)
+			}
+
+			return actionResult
 		},
 	},
 }
@@ -42,11 +83,13 @@ func main() {
 }
 
 func showHelp() {
-	fmt.Printf("%-20s\t%-40s\t%-20s\n", "Action", "Description", "Usage")
+	rows := [][]string{{"Action", "Description", "Usage"}}
 
 	for name, action := range actions {
-		fmt.Printf("%-20s\t%-40s\t%-20s\n", name, action.description, action.usage)
+		rows = append(rows, []string{name, action.description, action.usage})
 	}
+
+	prettyPrintTable(rows, 5, nil)
 
 	os.Exit(0)
 }
@@ -60,9 +103,74 @@ func run(actionName string, args []string) {
 
 	code := actionData.action(args)
 
-	if code == RESULT_INVALID_USAGE {
-		fmt.Println("Usage: ", actionData.usage)
+	if code == RESULT_SUCCESS {
+		fmt.Println("Done")
+	} else if code == RESULT_INVALID_USAGE {
+		fmt.Println("Invalid usage! Usage: ", actionData.usage)
+	} else {
+		fmt.Println("Something went wrong! Code: ", code)
 	}
 
 	os.Exit(int(code))
+}
+
+func runCommand(workingDir, command string, outputWriter, errorWriter io.Writer) ActionResult {
+	if outputWriter == nil {
+		outputWriter = os.Stdout
+	}
+
+	if errorWriter == nil {
+		errorWriter = os.Stderr
+	}
+
+	commandParts := strings.Split(command, " ")
+
+	cmd := exec.Command(commandParts[0], commandParts[1:]...)
+	cmd.Dir = filepath.Join(strings.Split(workingDir, "/")...)
+
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		fmt.Fprintf(errorWriter, "Error creating StdoutPipe: %v\n", err)
+		return RESULT_FAILURE
+	}
+
+	scanner := bufio.NewScanner(stdout)
+
+	cmd.Start()
+
+	for scanner.Scan() {
+		fmt.Fprintln(outputWriter, scanner.Text())
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Fprintf(errorWriter, "Error waiting for command: %v\n", err)
+		return RESULT_FAILURE
+	}
+
+	return RESULT_SUCCESS
+}
+
+func prettyPrintTable(rows [][]string, gap int, outputWriter io.Writer) {
+	gap = max(gap, 1)
+
+	if outputWriter == nil {
+		outputWriter = os.Stdout
+	}
+
+	colSizes := make([]int, len(rows[0]))
+
+	for _, row := range rows {
+		for i, cell := range row {
+			colSizes[i] = max(colSizes[i], len(cell))
+		}
+	}
+
+	for _, row := range rows {
+		for i, cell := range row {
+			fmt.Fprintf(outputWriter, fmt.Sprintf("%%-%ds", colSizes[i]+gap), cell)
+		}
+
+		fmt.Println()
+	}
 }
